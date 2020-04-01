57f7caac75c2cfcd9f3ac1d99fdd5913f82799de
AlexandrDorosh <57454028+AlexandrDorosh@users.noreply.github.com>
2020-04-01 21:12:17 +0300

    loops

---
+++ Lesson_1/start.py
@@ -0,0 +1,120 @@
+import random
+# a = 10 # int
+# print(a, type (a))
+
+# a = 10.6 # float
+# print(a, type (a))
+
+# a = True # bool
+# print(a, type (a))
+
+# a = "Bill" # str
+# print(a, type (a))
+
+# name = input("Your name: ")
+# age = int(input("Your age: "))
+# print(name, type(name))
+# print(age, type(age))
+
+# a = int(input("Number 1: "))
+# b = int(input("Number 2: "))
+# c = int(input("Number 2: "))
+
+# sum = a + b
+# mult = a * b
+# dev = a / b
+# min = a - b
+# print("sum = ", sum)
+# print("mult = ", mult)
+# print("dev = ", dev)
+# print("min = ", min)
+
+# if a > b:
+#     print("a > b")
+# elif a < b:
+#     print("a < b")
+# else:
+#     print("a = b")
+
+# if a > b and a > c:
+#     print('a')
+# elif b > a or b > c:
+#     print("b")
+# else:
+#     print("c")
+
+
+# a = int(input("enter km: "))
+# b = a * 1000
+# print("result: ", b, 'm')
+
+# rnd = random.randrange(-10, 10)
+# print(rnd)
+
+
+# a = int(input("Введіть сумму"))
+
+# EURO = a * 30.10
+# USD = a * 28.70
+# RUB = a * 0.38
+
+# res1 = print(EURO)
+# res2 = print(USD)
+# res3 = print(RUB)
+
+
+# a = input
+
+# import random
+# num1 = random.randrange(0,20)
+# num2 = random.randrange(0,20)
+# num3 = random.randrange(0,20)
+# print(num1, num2, num3)
+# if num1 == num2 and num1 == num3 and num2 == num3:
+#     print("Rivni")
+# else:
+#     print("Ne rivni")
+
+
+# exit = False
+
+# while not exit:
+#     choice = int(input('1. Add\n2. Div\n0. Exin\n==>'))
+#     if choice == 1:
+#         print('a + b')
+#     elif choice == 2:
+#         print("a / b")
+#     elif choice == 0:
+#         exit = True
+
+
+# a = int(input("Виберіть 1 число"))
+# b = int(input("Виберіть 2 число"))
+# c = input("+, -, *, /")
+# if c = +:
+#     print(a + b)
+# elif c = -:
+#     print(a - b)
+# elif c = *:
+#     print(a * b)
+# elif c = /:
+#     print(a / b)
+# else:
+#     print("error!!!")
+
+
+# i = 0
+# counter = 0
+# while i < 7:
+#     i += 1
+#     temp = random.randrange(-10, 30)
+#     if temp > 10:
+#         counter += 1
+#     print(temp)
+# print("More then 10 => ", counter)
+
+
+# number = 8
+
+# for i in range(1, number):
+#     print(i)
